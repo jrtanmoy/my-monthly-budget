37e6a54e3ed7dd23c94dee04ee15c67990e3d053
jrtanmoy <roy28301@gmail.com>
2022-02-17 02:58:15 +0600

    remaining balance updated

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function inputValue(inputId){
     const inputValueText = inputField.value;
     const inputValueNumber = parseFloat(inputValueText);
     // clear input field
-    inputField.value = '';
+    // inputField.value = '';
     return inputValueNumber;
 }
 
@@ -26,17 +26,31 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     totalBalance.innerText = newBalance;
 })
 document.getElementById('save-btn').addEventListener('click', function(){
+    // get income value
+    const incomeAmount = inputValue('income-input');
+    // get food value
+    const foodAmount = inputValue('food-input')
+    // get rent value
+    const rentAmount = inputValue('rent-input')
+    // get clothes value
+    const clothesAmount = inputValue('clothes-input')
+    // set total expenses
+    const totalExpenses = document.getElementById('total-expenses');
+    const totalCost = foodAmount + rentAmount + clothesAmount;
+    totalExpenses.innerText = totalCost;
+    // set balance
+    const totalBalance = document.getElementById('total-balance');
+    const newBalance = incomeAmount - totalCost;
+    totalBalance.innerText = newBalance;
     // saving amount calculation
     const savingNumber = inputValue('saving-id');
     const savingPercentage = savingNumber / 100;
-    const incomeAmount = inputValue('income-input');
     const savingTotal = incomeAmount * savingPercentage;
     // set saving amount
     const totalSavingAmount = document.getElementById('total-saving');
     totalSavingAmount.innerText = savingTotal;
     // set remaining balance
     const remainingBalance = document.getElementById('total-remaining');
-    const newRemainingBalance = incomeAmount - savingTotal;
+    const newRemainingBalance = newBalance - savingTotal;
     remainingBalance.innerText = newRemainingBalance;
-
 })
